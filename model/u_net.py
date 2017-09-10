8d41a94a91c9ab04b892c5bf905cf9297365e914
paul <dezmoanded@gmail.com>
2017-09-10 19:26:21 -0400

    D

--- model/u_net.py
+++ model/u_net.py
@@ -431,13 +431,13 @@ def get_unet_1024(input_shape=(1024, 1024, 3),
     inputs = Input(shape=input_shape)
     # 1024
 
-    down0b, down0b_pool = down_layer(inputs, 64 * num_features_mul)
+    down0b, down0b_pool = down_layer(inputs, 32 * num_features_mul)
     # 512
 
-    down0a, down0a_pool = down_layer(down0b_pool, 64 * num_features_mul)
+    down0a, down0a_pool = down_layer(down0b_pool, 32 * num_features_mul)
     # 256
 
-    down0, down0_pool = down_layer(down0a_pool, 64 * num_features_mul)
+    down0, down0_pool = down_layer(down0a_pool, 32 * num_features_mul)
     # 128
 
     down1, down1_pool = down_layer(down0_pool, 64 * num_features_mul)
@@ -472,13 +472,13 @@ def get_unet_1024(input_shape=(1024, 1024, 3),
     up1 = up_layer(up2, down1, 64 * num_features_mul)
     # 128
 
-    up0 = up_layer(up1, down0, 64 * num_features_mul)
+    up0 = up_layer(up1, down0, 32 * num_features_mul)
     # 256
 
-    up0a = up_layer(up0, down0a, 64 * num_features_mul)
+    up0a = up_layer(up0, down0a, 32 * num_features_mul)
     # 512
 
-    up0b = up_layer(up0a, down0b, 64 * num_features_mul)
+    up0b = up_layer(up0a, down0b, 32 * num_features_mul)
     # 1024
 
     classify = Conv2D(num_classes, (1, 1), activation='sigmoid')(up0b)
